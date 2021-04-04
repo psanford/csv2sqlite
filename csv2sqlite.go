122cbdf3fc7a80b0ce39dfc7cae25fadc48a6f57
Peter Sanford <psanford@sanford.io>
2021-04-04 07:55:30 -0700

    Initial commit
    This is a simple tool to import csv files into sqlite. It offers a few
    niceties over `.headers on; .mode csv; .import foo.csv csv`:
    
    - it normalizes column names: removing symbols and downcasing
    - it can import multiple csvs properly (it won't add the header as a row)
    - it can smartly add columns to the table if there are new columns
      present in subsequent files

---
+++ csv2sqlite.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	db            = flag.String("db", "csv.db", "Database file")
+	tableName     = flag.String("table", "csv", "Table name")
+	createColumns = flag.Bool("create-columns", true, "Create any missing columns in table")
+	trunc         = flag.Bool("trunc", false, "Truncate table before inserting")
+)
+
+func main() {
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		log.Fatalf("usage: %s <input.csv>", os.Args[0])
+	}
+
+	if *trunc {
+		truncateTable()
+	}
+
+	for _, filename := range args {
+		processCSV(filename)
+	}
+}
+
+func truncateTable() {
+	db, err := sql.Open("sqlite3", *db)
+	if err != nil {
+		log.Fatalf("open db err: %s", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s", *tableName))
+	if err != nil {
+		log.Fatalf("Drop table err: %s", err)
+	}
+}
+
+func processCSV(filename string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("open file err: %s", err)
+	}
+	defer f.Close()
+
+	db, err := sql.Open("sqlite3", *db)
+	if err != nil {
+		log.Fatalf("open db err: %s", err)
+	}
+	defer db.Close()
+
+	r := csv.NewReader(f)
+	header, err := r.Read()
+	if err != nil {
+		log.Fatalf("csv read header err: %s", err)
+	}
+
+	missingHeaders := make(map[string]struct{})
+	regex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	for i, h := range header {
+		h = strings.ToLower(h)
+		h = regex.ReplaceAllString(h, "_")
+		header[i] = h
+		missingHeaders[h] = struct{}{}
+	}
+
+	createStmt := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (%s)", *tableName, strings.Join(header, ",\n"))
+	_, err = db.Exec(createStmt)
+	if err != nil {
+		log.Fatalf("Create table err: %s (%s)", err, createStmt)
+	}
+
+	rows, err := db.Query(fmt.Sprintf("PRAGMA table_info(%s)", *tableName))
+	if err != nil {
+		log.Fatalf("query table_info err: %s", err)
+	}
+
+	for rows.Next() {
+		var (
+			cid        interface{}
+			name       string
+			colType    string
+			notnull    interface{}
+			dflt_value interface{}
+			pk         interface{}
+		)
+
+		err = rows.Scan(&cid, &name, &colType, &notnull, &dflt_value, &pk)
+		if err != nil {
+			log.Fatalf("Scan table_info err: %s", err)
+		}
+
+		delete(missingHeaders, name)
+	}
+	err = rows.Close()
+	if err != nil {
+		log.Fatalf("query table_info err: %s", err)
+	}
+
+	for k := range missingHeaders {
+		_, err = db.Exec(fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s", *tableName, k))
+		if err != nil {
+			log.Fatalf("Add column %s err: %s", k, err)
+		}
+	}
+
+	qs := strings.Repeat("?,", len(header))
+	qs = qs[:len(qs)-1]
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare(fmt.Sprintf("INSERT INTO %s (%s) values (%s)", *tableName, strings.Join(header, ","), qs))
+	if err != nil {
+		log.Fatalf("Prepare insert err: %s", err)
+	}
+
+	rowFace := make([]interface{}, len(header))
+	for {
+		line, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
+		for i, v := range line {
+			rowFace[i] = v
+		}
+
+		_, err = stmt.Exec(rowFace...)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
